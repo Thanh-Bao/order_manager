dbc9495ebeb820caeea69eed9ae9fe6a75b249db
Bảo Bảo <thanh.bao@outlook.com>
2023-05-14 20:19:47 +0700

    Demo Detail page

---
+++ order_manager/webapp/controller/Detail.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+
+    return Controller.extend("ordermanager.controller.Detail", {
+
+        onEditToggleButtonPress: function () {
+            var oObjectPage = this.getView().byId("ObjectPageLayout"),
+                bCurrentShowFooterState = oObjectPage.getShowFooter();
+
+            oObjectPage.setShowFooter(!bCurrentShowFooterState);
+        }
+    });
+});
